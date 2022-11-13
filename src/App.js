49beb1fd0d04e4e2d636962bd5281e1aad27be16
Ian McQuillan <ian_mcquillan@msn.com>
2022-11-12 18:20:13 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Nav from './Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: null,
+      post: [],
+      name: 'Ian',
+      age: 25
+    }
+    console.log('Construction complete...')
+  }
+
+  componentDidMount = () => {
+    console.log('First rendering complete!')
+  }
+
+  happyBirthday = () => {
+    console.log('Adding to age...')
+    this.setState({age: this.state.age + 1})
+  }
+
+  render() {
+    console.log('Rendering...')
+    return (
+      <div>
+        <Nav/>
+        <h1>Hello</h1>
+        <h3>The name is {this.state.name}</h3>
+        <button onClick={this.happyBirthday} >Add to age +</button>
+        <h3>Age: {this.state.age}</h3>
+      </div>
+    )
+  }
 }
-
-export default App;
